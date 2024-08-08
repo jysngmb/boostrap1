a769e7e8855eb11d83b0d6765947946d302f919d
jysngmb <jaysongomba@gmail.com>
2024-08-09 01:58:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function goTo(page) {
+  document.querySelector("body").style.opacity = 0;
+
+  setTimeout(function () {
+    window.location.href = page;
+  }, 500);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.querySelector("body").style.opacity = 1;
+  }, 50);
+});
